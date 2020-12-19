e9dd402d00bdfc302a4b0a4dbc1304e9eb6d47e2
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2020-12-19 17:59:32 +0200

    Prepare markoff project to migrate to another GitHub project space

--- markoff/src/main/java/mayton/compression/Constants.java
+++
@@ -1,9 +0,0 @@
-package mayton.compression;
-
-public class Constants {
-
-    public static final String EXCHANGE_NAME = "exchange1";
-    public static final String ROUTING_KEY = "color";
-    public static final String QUEUE_NAME = "queue1";
-
-}
